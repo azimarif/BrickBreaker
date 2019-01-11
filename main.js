60a797bebd3606f5f8d1157d10f2d6bd250a9406
Arif <44020819+azimarif@users.noreply.github.com>
2019-01-11 19:28:59 +0530

    Add Bricks on the screen
    Bricks and Screen are now seperate entities.

--- main.js
+++ main.js
@@ -11,11 +11,11 @@ const drawPaddle = function (paddleDiv, paddle) {
   paddleDiv.style.height = addSuffixPixel(paddle.height);
   paddleDiv.style.left = addSuffixPixel(paddle.left);
   paddleDiv.style.bottom = addSuffixPixel(paddle.bottom);
-}
+};
 
 const createPaddle = function (document, paddle) {
   let paddleDiv = document.createElement('div');
-  paddleDiv.id = 'paddleDiv'
+  paddleDiv.id = 'paddleDiv';
   drawPaddle(paddleDiv, paddle);
   document.getElementById('gameScreen').appendChild(paddleDiv);
   document.getElementById('gameScreen').focus();
@@ -24,24 +24,55 @@ const createPaddle = function (document, paddle) {
 
 const handleEvent = function (paddleDiv, paddle) {
   if (event.keyCode == LEFT_KEY) {
-    paddle.moveLeft();
-    if (paddle.left >= 0) {
+    if (paddle.left > 0) {
+      paddle.moveLeft();
       drawPaddle(paddleDiv, paddle);
     }
   }
   if (event.keyCode == RIGHT_KEY) {
-    paddle.moveRight();
-    if (paddle.left <= 860) {
-      console.log(paddle.left);
+    if (paddle.left <= 850) {
+      paddle.moveRight();
       drawPaddle(paddleDiv, paddle);
     }
   }
-}
+};
+
+const createScreen = function (document, screen) {
+  let body = document.getElementsByTagName('body')[0];
+  let screenElement = document.createElement('main');
+  screenElement.id = 'gameScreen';
+  screenElement.tabIndex = '0';
+  screenElement.className = 'screen';
+  screenElement.style.width = addSuffixPixel(screen.width);
+  screenElement.style.height = addSuffixPixel(screen.height);
+  body.appendChild(screenElement);
+
+  return screenElement;
+};
+
+const drawBricks = function (bricks, screen) {
+  bricks.forEach(brick => {
+    let brickElement = document.createElement('div');
+    brickElement.style.width = addSuffixPixel(brick.width);
+    brickElement.style.height = addSuffixPixel(brick.height);
+    brickElement.style.top = addSuffixPixel(brick.top);
+    brickElement.style.left = addSuffixPixel(brick.left);
+    brickElement.className = 'bricks';
+    screen.appendChild(brickElement);
+  });
+};
 
 const initialize = function () {
   let paddle = new Paddle(100, 20, 430, 5);
+  let screen = new Screen(960, 600);
+  let screenElement = createScreen(document, screen);
   let paddleDiv = createPaddle(document, paddle);
-  document.getElementById('gameScreen').onkeydown = () => { handleEvent(paddleDiv, paddle); }
+  let bricks = new Bricks(9, 6, 70, 25);
+
+  drawBricks(bricks.createBricks(), screenElement);
+  document.getElementById('gameScreen').onkeydown = () => {
+    handleEvent(paddleDiv, paddle);
+  };
 };
 
 window.onload = initialize;
--- model.js
+++ model.js
@@ -14,3 +14,47 @@ class Paddle {
     this.left = this.left + 10;
   }
 }
+
+class Brick {
+  constructor(width, height, left, top, status) {
+    this.width = width;
+    this.height = height;
+    this.left = left;
+    this.top = top;
+    this.status = status;
+  }
+}
+
+class Bricks {
+  constructor(rows, columns, width, height, horizontalMargin = 32, verticalMargin = 15) {
+    this.numberOfBricksPerRow = rows;
+    this.numberOfBricksRow = columns;
+    this.width = width;
+    this.height = height;
+    this.horizontalMargin = horizontalMargin;
+    this.verticalMargin = verticalMargin;
+  }
+
+  createBricks() {
+    let bricks = [];
+    for (let row = 0; row < this.numberOfBricksRow; row++) {
+      this.createBricksByRow(row, bricks);
+    }
+    return bricks;
+  }
+
+  createBricksByRow(row, bricks) {
+    for (let column = 0; column < this.numberOfBricksPerRow; column++) {
+      let left = column * (this.width + this.horizontalMargin) + this.horizontalMargin;
+      let top = row * (this.height + this.verticalMargin) + this.verticalMargin;
+      bricks.push(new Brick(this.width, this.height, left, top, true));
+    }
+  }
+}
+
+class Screen {
+  constructor(width, height) {
+    this.width = width;
+    this.height = height;
+  }
+}
